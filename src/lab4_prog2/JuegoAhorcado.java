da727a562968ac76951ee705c77cac435b8499c0
duglo <doufy209@gmail.com>
2026-08-15 10:30:21 -0600

    se modifico cosas en las reglas generales del juego

--- src/lab4_prog2/JuegoAhorcado.java
+++ src/lab4_prog2/JuegoAhorcado.java
@@ -1,38 +1,49 @@
 package lab4_prog2;
 import java.util.ArrayList;
 import java.util.List;
+
 public abstract class JuegoAhorcado {
     protected String palabraSecreta;
     protected char[] palabraMostrada;
     protected int intentosRestantes;
-    protected final int Maxintentos = 6;
+    protected final int MAX_INTENTOS = 6;
     protected List<Character> letrasIngresadas;
     protected List<String> figuraAhorcado;
+
     public JuegoAhorcado() {
-        this.intentosRestantes = Maxintentos;
+        this.intentosRestantes = MAX_INTENTOS;
         this.letrasIngresadas = new ArrayList<>();
         this.figuraAhorcado = new ArrayList<>();
     }
+
     public abstract void actualizarPalabraMostrada(char letra);
     public abstract boolean verificarLetra(char letra);
     public abstract boolean determinarVictoria();
-    public boolean procesarJugada(char letra) throws LetraInvalidaException, LetraRepetidaException {
+
+    public boolean procesarJugada(char letra) throws IllegalArgumentException {
+        
         if (!Character.isLetter(letra)) {
-            throw new LetraInvalidaException("Debes ingresar solo una letra, sin números ni símbolos.");
+            throw new IllegalArgumentException("Error: El carácter '" + letra + "' no es válido. Ingresa solo letras.");
         }
+
         char letraNormalizada = normalizarLetra(letra);
+
         if (letrasIngresadas.contains(letraNormalizada)) {
-            throw new LetraRepetidaException("Ya ingresaste la letra '" + letra + "' antes.");
+            return false; 
         }
+
         letrasIngresadas.add(letraNormalizada);
+
         if (verificarLetra(letraNormalizada)) {
             actualizarPalabraMostrada(letraNormalizada);
-            return true;
         } else {
             intentosRestantes--;
-            return false;
+            figuraAhorcado.add("error_" + figuraAhorcado.size());
         }
+
+        return true; 
     }
+
     protected char normalizarLetra(char c) {
         c = Character.toLowerCase(c);
         switch (c) {
@@ -40,12 +51,21 @@ public abstract class JuegoAhorcado {
             case 'é': return 'e';
             case 'í': return 'i';
             case 'ó': return 'o';
-            case 'ú': return 'u';
+            case 'ú': case 'ü': return 'u';
             default: return c;
         }
     }
-    public int getIntentosRestantes() { return intentosRestantes; }
-    public char[] getPalabraMostrada() { return palabraMostrada; }
-    public List<Character> getLetrasIngresadas() { return letrasIngresadas; }
-    public List<String> getFiguraAhorcado() { return figuraAhorcado; }
+
+    public int getIntentosRestantes() {
+        return intentosRestantes; 
+    }
+    public char[] getPalabraMostrada() { 
+        return palabraMostrada; 
+    }
+    public List<Character> getLetrasIngresadas() { 
+        return letrasIngresadas;
+    }
+    public List<String> getFiguraAhorcado() { 
+        return figuraAhorcado;
+    }
 }

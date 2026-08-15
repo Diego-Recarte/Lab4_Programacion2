59f5626715b0b98a0fcf266cc2098f30bde546c8
DEaniel Kattan <dagkattan221608@gmail.com>
2026-08-15 09:10:27 -0600

    Primer parte de errores

---
+++ src/lab4_prog2/LetraRepetidaException.java
@@ -0,0 +1,8 @@
+package lab4_prog2;
+
+public class LetraRepetidaException extends Exception {
+
+    public LetraRepetidaException(String mensaje) {
+        super(mensaje);
+    }
+}
---
+++ src/lab4_prog2/PalabraRepetidaException.java
@@ -0,0 +1,8 @@
+package lab4_prog2;
+
+public class PalabraRepetidaException extends Exception {
+
+    public PalabraRepetidaException(String mensaje) {
+        super(mensaje);
+    }
+}

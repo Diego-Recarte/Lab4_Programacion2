db5061f49451c413d08aadd593f33e791d121971
duglo <doufy209@gmail.com>
2026-08-15 09:23:20 -0600

    administrador palabras

--- src/lab4_prog2/AdministradorPalabras.java
+++ src/lab4_prog2/AdministradorPalabras.java
@@ -1,13 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package lab4_prog2;
 
-/**
- *
- * @author doufy
- */
+package lab4_prog2;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class AdministradorPalabras {
-    
+    private List<String> palabrasDisponibles;
+    private Random random;
+
+    public AdministradorPalabras() {
+        this.palabrasDisponibles = new ArrayList<>();
+        this.random = new Random();
+    }
+
+    public void agregarPalabra(String nuevaPalabra) {
+        if (!palabrasDisponibles.contains(nuevaPalabra)) {
+            palabrasDisponibles.add(nuevaPalabra);
+        } else {
+        }
+    }
+
+    public String obtenerPalabraAzar() {
+        if (palabrasDisponibles.isEmpty()) {
+            return null; 
+        }
+        
+        int indice = random.nextInt(palabrasDisponibles.size());
+        return palabrasDisponibles.get(indice);
+    }
 }
